71605870b8fb398f44e44c7b84b5403484b39a7c
limajpp <joaopedrocal90@gmail.com>
2026-03-28 20:10:47 -0300

    first commit

---
+++ av1-backend/av1/src/test/java/com/arqsistemas/av1/Av1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arqsistemas.av1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Av1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
